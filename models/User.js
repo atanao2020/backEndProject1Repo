7aadd40c8edb41a282eee69fca3307e52ee7ce82
Ari Tanao <ari.tanao2k19@gmail.com>
2020-11-03 08:50:17 +1000

    First Commit for Back End Group First Project

---
+++ models/User.js
@@ -0,0 +1,20 @@
+
+const mongoose = require('mongoose')
+const passportLocalMongoose = require("passport-local-mongoose")
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    fullName:{
+        type:String,
+        required:true
+    }
+})
+
+userSchema.plugin(passportLocalMongoose)
+module.exports = mongoose.model("users", userSchema)
---
+++ server.js
@@ -0,0 +1,99 @@
+const express               =  require('express'),
+      app                   =  express(),
+      port                  = 2050,
+      mongoose              =  require("mongoose"),
+      multer                = require('multer'),
+      upload                = multer(),
+      cors                  = require('cors'),
+      passport              =  require("passport"),
+      bodyParser            =  require("body-parser"),
+      LocalStrategy         =  require("passport-local"),
+      passportLocalMongoose =  require("passport-local-mongoose"),
+      User                 =  require("./models/User")
+
+//================================================== MongooseDB Connection
+mongoose.connect('mongodb+srv://atanao:dontinon@cluster0.enweg.mongodb.net/viewersDB?retryWrites=true&w=majority', 
+{useNewUrlParser:true,useUnifiedTopology:true},
+    function(err,database){
+        if(err){
+           throw err
+        }
+        console.log("connection made to database")
+   }
+)
+
+//===================================================
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(upload.array())
+app.use(express.json())
+
+
+//=====================================================
+app.use(require("express-session")({
+    secret:"Any normal Word",       //decode or encode session
+    resave: false,          
+    saveUninitialized:false    
+}));
+
+passport.serializeUser(User.serializeUser());       //session encoding
+passport.deserializeUser(User.deserializeUser());   //session decoding
+passport.use(new LocalStrategy(User.authenticate()));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({ extended:true, useUnifiedTopology:true }))
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//======================= Routes
+app.get("/", (req,res) =>{
+    res.render("homePage");
+})
+
+app.get("/userProfile",isLoggedIn ,(req,res) =>{
+    res.render("userProfile");
+})
+//Auth Routes
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.post("/login",passport.authenticate("local",{
+    successRedirect:"/userProfile",
+    failureRedirect:"/login"
+}),function (req, res){
+
+});
+
+app.get("/signIn",(req,res)=>{
+    res.render("signIn");
+});
+
+app.post("/signIn",(req,res)=>{
+    
+    User.register(new User({username: req.body.username,fullName:req.body.fullName}),req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.render("signIn");
+        }
+    passport.authenticate("local")(req,res,function(){
+        res.redirect("/login");
+    })    
+    })
+})
+
+app.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req,res,next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+app.listen(port, () => {   
+    console.log(`Example app listening at http://localhost:${port}`) 
+})
